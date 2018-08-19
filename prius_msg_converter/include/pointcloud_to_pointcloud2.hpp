eaa85c204f811125b9d8634ce80fbe4e06d93b00
RyodoTanaka <groadpg@gmail.com>
2018-08-19 21:41:56 +0900

    [update] modify file structure

--- prius_msg_converter/include/pointcloud_to_pointcloud2.hpp
+++ prius_msg_converter/include/pointcloud_to_pointcloud2.hpp
@@ -1,5 +1,5 @@
-#ifndef REPUB_PC2_HPP_INCLUDED
-#define REPUB_PC2_HPP_INCLUDED
+#ifndef POINTCLOUD_TO_POINTCLOUD2_HPP_INCLUDED
+#define POINTCLOUD_TO_POINTCLOUD2_HPP_INCLUDED
 
 #include <ros/ros.h>
 
@@ -7,15 +7,17 @@
 
 namespace prius {
   namespace msg_converter {
-    class RePubPC2 {
+    class PCtoPC2 {
     public:
-      RePubPC2();
-      ~RePubPC2();
+      PCtoPC2();
+      ~PCtoPC2();
     private:
       void PCCallback(const sensor_msgs::PointCloud::ConstPtr &msg);
 
       ros::NodeHandle nh_;
 
+      ros::Subscriber pc2_sub_;
+      
       ros::Publisher pc2_msg_pub_;
     };
   }
---
+++ prius_msg_converter/src/pointcloud_to_pointcloud2.cpp
@@ -0,0 +1,33 @@
+#include <pointcloud_to_pointcloud2.hpp>
+#include <ros/console.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+
+namespace pmc = prius::msg_converter;
+using namespace std;
+
+pmc::PCtoPC2::PCtoPC2()
+{
+  pc2_sub_ = nh_.subscribe("/prius/center_laser/scan", 1, &pmc::PCtoPC2::PCCallback, this);
+  pc2_msg_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/prius/center_laser/scan2", 1);
+
+  ROS_INFO("point cloud to point cloud2 converter is ready.");
+
+}
+
+pmc::PCtoPC2::~PCtoPC2(){ }
+
+void pmc::PCtoPC2::PCCallback(const sensor_msgs::PointCloud::ConstPtr &msg)
+{
+  sensor_msgs::PointCloud2 pub_msg;
+  sensor_msgs::convertPointCloudToPointCloud2(*msg, pub_msg);
+
+  pc2_msg_pub_.publish(pub_msg);
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "point_cloud_to_point_cloud2_node");
+  pmc::PCtoPC2 rpp2;
+  ros::spin();
+}
